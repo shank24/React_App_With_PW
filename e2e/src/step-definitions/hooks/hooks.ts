6ed12498723e46098b88434cf27a785822ace84e
Shanky Kalra <shankkalra@gmail.com>
2024-05-18 23:45:06 -0300

    Code Add

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -1,5 +1,7 @@
 import {BeforeAll, Before, AfterAll, After} from "@cucumber/cucumber";
-const {chromium} = require("playwright");
+import { chromium } from "playwright";
+
+//const chromium = require("playwright");
 
 BeforeAll(async() => {
     global.browser = await chromium.launch({
