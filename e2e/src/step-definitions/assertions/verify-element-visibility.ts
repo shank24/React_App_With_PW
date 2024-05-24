55f4a872d48fd6ca109e923ebb4ef98729fb9d3e
Shanky Kalra <shankkalra@gmail.com>
2024-05-23 22:39:33 -0300

    Locator

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -10,6 +10,14 @@ Then(
     }
 );
 
+Then(
+    /^the "([^"]*)" should be displayed$/,
+    async function name(elementKey: string) {
+        console.log(`the ${elementKey} should be displayed`);
+        const locator = await global.page.locator(".testing-talks-logo");
+        expect(locator).toBeVisible();
+    }
+);
 /*
 Then('the {string} should contain the text {string}', async function (elementKey: string, expectedElement: string) {
     console.log(`the ${elementKey} should contain the text ${expectedElement}`);
