164158ae7a44dc3a1e2f44162a7109727a5a50d6
Shanky Kalra <shankkalra@gmail.com>
2024-06-05 22:46:15 -0300

    Cucumber World

--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -1,34 +1,32 @@
-import { BeforeAll, Before, AfterAll, After } from "@cucumber/cucumber";
-//import { chromium } from "playwright";
+import { Before, After, ITestCaseHookParameter } from "@cucumber/cucumber";
+import { ScenarioWorld } from "./world";
 
-const {chromium} = require("playwright");
-
-BeforeAll(async () => {
-    global.browser = await chromium.launch({
-        headless: false,
-    })
-});
-
-AfterAll(async () => {
-    await global.browser.close();
-});
-
-Before(async (scenario) => {
-    global.context = await global.browser.newContext({
+Before(async function (this: ScenarioWorld, scenario) {
+    console.log(`Running Cucumber Scenario ${scenario.pickle.name}`)
+    const contextOptions = {
         recordVideo: {
             //dir: './reports/videos/'+scenario.pickle.name,
             dir: `./reports/videos/'${scenario.pickle.name}`
         }
-    });
-    global.page = await global.context.newPage();
+    }
+
+    const ready = await this.init(contextOptions);
+    return ready;
+
 });
 
-After(async (scenario) => {
+After(async function (this: ScenarioWorld, scenario) {
+    const {
+        screen: { page, browser }
+    } = this;
+
     const scenarioStatus = scenario.result?.status;
-    if(scenarioStatus === 'FAILED'){
-        await global.page.screenshot({
+    if (scenarioStatus === 'FAILED') {
+        await page.screenshot({
             path: `./reports/screenshots/${scenario.pickle.name}.png`
         })
     }
-    await global.page.close();
+    await browser.close();
+    return browser;
+    
 });
