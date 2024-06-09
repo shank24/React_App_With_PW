4ab71d2674d4fe645a9ad725babbe0292dc2a21d
Shanky Kalra <shankkalra@gmail.com>
2024-06-08 23:06:21 -0300

    ENV SET

--- e2e/dist/step-definitions/setup/hooks.js
+++ e2e/dist/step-definitions/setup/hooks.js
@@ -2,6 +2,8 @@
 
 var _cucumber = require("@cucumber/cucumber");
 
+var _parseEnv = require("../../env/parseEnv");
+
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
@@ -15,7 +17,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
           console.log("Running Cucumber Scenario ".concat(scenario.pickle.name));
           contextOptions = {
             recordVideo: {
-              dir: "./reports/videos/'".concat(scenario.pickle.name)
+              dir: "".concat((0, _parseEnv.env)('VIDEO_PATH')).concat(scenario.pickle.name)
             }
           };
           _context.next = 4;
@@ -55,7 +57,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
           _context2.next = 5;
           return page.screenshot({
-            path: "./reports/screenshots/".concat(scenario.pickle.name, ".png")
+            path: "".concat((0, _parseEnv.env)('SCREENSHOT_PATH')).concat(scenario.pickle.name, ".png")
           });
 
         case 5:
--- e2e/src/step-definitions/setup/hooks.ts
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -1,12 +1,13 @@
 import { Before, After, ITestCaseHookParameter } from "@cucumber/cucumber";
 import { ScenarioWorld } from "./world";
+import {env} from '../../env/parseEnv'
 
 Before(async function (this: ScenarioWorld, scenario) {
     console.log(`Running Cucumber Scenario ${scenario.pickle.name}`)
 
     const contextOptions = {
         recordVideo: {
-            dir: `./reports/videos/'${scenario.pickle.name}`
+            dir: `${env('VIDEO_PATH')}${scenario.pickle.name}`
         }
     }
 
@@ -24,7 +25,7 @@ After(async function (this: ScenarioWorld, scenario) {
     const scenarioStatus = scenario.result?.status;
     if (scenarioStatus === 'FAILED') {
         await page.screenshot({
-            path: `./reports/screenshots/${scenario.pickle.name}.png`
+            path: `${env('SCREENSHOT_PATH')}${scenario.pickle.name}.png`
         });
     }
     await browser.close();
