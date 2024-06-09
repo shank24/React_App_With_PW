d76f44df8785f9225fcb0df043397e238ee073b9
Shanky Kalra <shankkalra@gmail.com>
2024-06-08 22:52:49 -0300

    ENV SET

--- e2e/dist/index.js
+++ e2e/dist/index.js
@@ -4,6 +4,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.smoke = exports.regression = exports.dev = void 0;
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _parseEnv = require("./env/parseEnv");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv.default.config({
+  path: (0, _parseEnv.env)('COMMON_CONFIG_FILE')
+});
+
 var common = "./src/features/**/*.feature                 --require-module ts-node/register                 --require ./src/step-definitions/**/**/*.ts                 -f json:./reports/report.json                 --format progress-bar";
 var dev = exports.dev = "".concat(common, " --tags '@dev'");
 var smoke = exports.smoke = "".concat(common, " --tags '@smoke'");
